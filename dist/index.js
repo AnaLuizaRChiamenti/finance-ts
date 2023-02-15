4a87330127dcaa16139e0221f4de59e06d607907
Ana Luiza Rodrigues Chiamenti <Analuizarodrigueschiamenti@gmail.com>
2023-02-15 15:53:39 -0300

    criamos a funcao transaction

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    name: "Ana",
+    login: "AnaChiamenti",
+    enable: true,
+    password: "123"
+};
+const WalletUser = {
+    enable: true,
+    owner: user,
+    transactions: []
+};
+function addTransaction(wallet, transaction) {
+    wallet.transactions.push(transaction);
+}
+addTransaction(WalletUser, { description: "mercado", type: "D", value: 500 });
+addTransaction(WalletUser, { description: "padaria", type: "D", value: 150 });
+addTransaction(WalletUser, { description: "carro", type: "C", value: 1500 });
+console.log(WalletUser);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world!");
+import { Transaction, User, Wallet } from "./types";
+
+
+const user: User = { 
+    name: "Ana", 
+    login: "AnaChiamenti", 
+    enable: true, 
+    password: "123" }
+
+const WalletUser: Wallet = { 
+    enable: true, 
+    owner: user, 
+    transactions: [] };
+
+    function addTransaction(wallet: Wallet, transaction: Transaction) {
+        wallet.transactions.push(transaction)
+    }
+
+    addTransaction(WalletUser, {description:"mercado", type: "D", value: 500})
+    addTransaction(WalletUser, {description:"padaria", type: "D", value: 150})
+    addTransaction(WalletUser, {description:"carro", type: "C", value: 1500})
+
+    console.log(WalletUser);
+    
